750d1d53e2db4094c25855dfbb97682d31833f55
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-05-31 11:15:30 -0300

    Package for PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ from jsonbender import __version__
 setup(
     name='JSONBender',
     version=__version__,
-    description='Library for transforming dicts.',
-    packages=find_packages(),
+    description='Library for transforming JSON data between different formats.',
+    author='Elias Tandel',
+    author_email='backend@onyo.com',
+    url='https://github.com/Onyo/jsonbender',
+    download_url='https://codeload.github.com/Onyo/jsonbender/tar.gz/' + __version__,
+    keywords=['dsl', 'edsl', 'json'],
+    packages=['jsonbender'],
 )
 
